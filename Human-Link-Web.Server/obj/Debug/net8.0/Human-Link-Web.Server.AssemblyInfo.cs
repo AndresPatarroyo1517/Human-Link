1d48145ef6996ed81ee5807f14cc82b805d4c4ec
AndresPatarroyo1517 <159494183+AndresPatarroyo1517@users.noreply.github.com>
2024-09-25 15:10:04 -0500

    .gitignore

--- Human-Link-Web.Server/obj/Debug/net8.0/Human-Link-Web.Server.AssemblyInfo.cs
+++ Human-Link-Web.Server/obj/Debug/net8.0/Human-Link-Web.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Human-Link-Web.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+050f2f05c0100c902b0af87b9014569d5a59b70e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d43143b2d54412d85d09940412f31fb59c6bfbb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Human-Link-Web.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Human-Link-Web.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

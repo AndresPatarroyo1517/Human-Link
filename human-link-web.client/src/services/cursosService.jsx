e640d68d4f52333de3918f45dab6d57f2634b708
Santiago1470 <st147086@gmail.com>
2024-10-13 20:26:33 -0500

    Cambios en el backend y frontend para cargar cursos - usuario
    Se comentó y cambió en el backend el endpoint GET{id} CursosUsuario para retornar lista de cursos según el id del usuario. En el frontend se creó cursosService para la petición GET de los cursos y se agregó la función useEffect() para cargar los cursos según el usuario (se debe cambiar para consultar por un id obtenido de las cookies del usuario logeado).

---
+++ human-link-web.client/src/services/cursosService.jsx
@@ -0,0 +1,25 @@
+const API_URL = 'https://localhost:7019/HumanLink/CursoUsuario';
+
+const cursosService = {
+
+    // Obtener cursos según el id del empleado
+    getCursosEmpleado: async (idEmpleado) => {
+        const response = await fetch(API_URL + "/" + idEmpleado, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include', // Enviar cookies con la solicitud
+            /*body: JSON.stringify(userData),*/ // Convertir el objeto a JSON
+        });
+
+        if (!response.ok) {
+            throw new Error('Error al consultar los cursos del usuario');
+        }
+
+        const data = await response.json();
+        return data;
+    } 
+}
+
+export default cursosService;

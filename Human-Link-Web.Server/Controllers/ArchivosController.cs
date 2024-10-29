6f2cf56c79127097aee7f036601e8e130a6ef81b
AndresPatarroyo1517 <159494183+AndresPatarroyo1517@users.noreply.github.com>
2024-10-28 20:13:22 -0500

    Arreglo del endpoint de Archivos

--- Human-Link-Web.Server/Controllers/ArchivosController.cs
+++ Human-Link-Web.Server/Controllers/ArchivosController.cs
@@ -1,39 +1,35 @@
 ﻿using Human_Link_Web.Server.Models;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using MongoDB.Driver.GridFS;
-using System.IO;
-using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace Human_Link_Web.Server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ArchivosController : ControllerBase
+    public class ArchivosController(HumanLink_Mongo context) : ControllerBase
     {
-        private readonly HumanLink_Mongo _context;
+        private readonly HumanLink_Mongo _context = context;
 
-        public ArchivosController(HumanLink_Mongo context)
+        [Authorize(Policy = "AllPolicy")]
+        [HttpPost]
+        public async Task<IActionResult> SubirArchivo(IFormFile file)
         {
-            _context = context;
-        }
-
-        [HttpPost("subir")]
-        public async Task<IActionResult> SubirArchivo(IFormFile file, string propietario)
-        {
+            var propietario = HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value;
             if (file == null || file.Length == 0)
                 return BadRequest("No se ha subido ningún archivo.");
 
-            // Guardar el archivo en GridFS
             using (var stream = file.OpenReadStream())
             {
-                var fileId = await _context.GridFS.UploadFromStreamAsync(file.FileName, stream);
-                var nuevoArchivo = new Archivo
+                ObjectId fileId = await _context.GridFS.UploadFromStreamAsync(file.FileName, stream);
+                Archivo nuevoArchivo = new()
                 {
-                    ArchivoPath = fileId.ToString(), // Guardar el ID de GridFS
-                    Propietario = propietario
+                    ArchivoPath = fileId.ToString(), 
+                    Propietario = propietario,
+                    NombreArchivo = (file.FileName).ToLower()
                 };
 
                 await _context.Archivos.InsertOneAsync(nuevoArchivo);
@@ -42,19 +38,38 @@ namespace Human_Link_Web.Server.Controllers
             return Ok(new { mensaje = "Archivo subido exitosamente" });
         }
 
+        [Authorize(Policy = "AllPolicy")]
         [HttpGet]
         public async Task<IActionResult> ObtenerArchivos()
         {
             var archivos = await _context.Archivos.Find(_ => true).ToListAsync();
+            var resultados = archivos.Select(archivo => new
+            {
+                Id = archivo.ArchivoPath,
+                Propietario = archivo.Propietario,
+                NombreArchivo = archivo.NombreArchivo
+            }).ToList();
+
+            return Ok(resultados);
+        }
+
+        [Authorize(Policy = "AllPolicy")]
+        [HttpGet("query/{nombreArchivo}")]
+        public async Task<IActionResult> ObtenerArchivosByName(string nombreArchivo)
+        {
+            var filter = Builders<Archivo>.Filter.Regex(a => a.NombreArchivo, new BsonRegularExpression($"^{nombreArchivo.ToLower()}.*", "i"));
+
+            var archivos = await _context.Archivos.Find(filter).ToListAsync();
+
             return Ok(archivos);
         }
 
+        [Authorize(Policy = "AllPolicy")]
         [HttpGet("descargar/{id}")]
         public async Task<IActionResult> DescargarArchivo(string id)
         {
             var fileId = new MongoDB.Bson.ObjectId(id);
 
-            // Obtener el archivo de GridFS
             var fileInfo = await _context.GridFS.Find(Builders<GridFSFileInfo<ObjectId>>.Filter.Eq(f => f.Id, fileId)).FirstOrDefaultAsync();
 
             if (fileInfo == null)
@@ -65,8 +80,9 @@ namespace Human_Link_Web.Server.Controllers
             await stream.CopyToAsync(result);
             result.Position = 0;
 
-            // Usar el nombre del archivo desde los metadatos
             return File(result, "application/octet-stream", fileInfo.Filename);
         }
+
+
     }
 }
--- Human-Link-Web.Server/Models/Archivo.cs
+++ Human-Link-Web.Server/Models/Archivo.cs
@@ -7,9 +7,9 @@ namespace Human_Link_Web.Server.Models
     {
         [BsonId]
         public ObjectId Id { get; set; }
-
         public required string ArchivoPath { get; set; }
         public required string Propietario { get; set; }
+        public required string NombreArchivo { get; set; }
         public DateTime Fecha { get; set; } = DateTime.UtcNow;
         public string Estado { get; set; } = "sin verificar";
     }
